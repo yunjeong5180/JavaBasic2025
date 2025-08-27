88a4092736dc68c685a34bc7be8ca4b5f91b14e1
yoonjeong <yonjeong5180@gmail.com>
2025-08-27 17:43:04 +0900

    08.27

---
+++ src/loop/Whilel2_2.java
@@ -0,0 +1,26 @@
+package loop;
+
+public class Whilel2_2
+{// 1부터 하나씩 증가하는 수를 3번 더해라 (1 ~ 3 더하기)
+    // 이 문제는 1부터 하나씩 증가하는 수이기 때문에 1 + 2 + 3을 더해야 한다.
+    //우선 while 문을 사용하지 않고 단순 무식하게 풀어보자.
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+
+        sum = sum + i;
+        System.out.println("i = " + i + " sum = " + sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i = " + i + " sum = " + sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i = " + i + " sum = " + sum);
+
+
+
+    }
+}
---
+++ src/question/loop/Ex1_for.java
@@ -0,0 +1,16 @@
+package question.loop;
+
+public class Ex1_for
+{// 자연수 출력
+    // 처음 10개의 자연수를 출력하는 프로그램을 작성해 보세요. 이때, count 라는 변수를 사용해야 합니다.
+    //while문, for문 2가지 버전의 정답을 만들어야 합니다.
+
+    public static void main(String[] args) {
+        int count = 10;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
