fa9c05ff7ef945066c053f8ecb4d28678f9e92c9
yoonjeong <yonjeong5180@gmail.com>
2025-08-29 01:12:47 +0900

    08.28

---
+++ src/array/ArrayDi3.java
@@ -0,0 +1,25 @@
+package array;
+
+public class ArrayDi3
+{
+    public static void main(String[] args)
+    { // 구조 개선 - 초기화, 배열의 길이
+
+        // 2x3 2차원 배열, 초기화
+        int [][] arr =
+                {
+                    {1,2,3},
+                    {4,5,6}
+                }; // 행(가로)2, 열(세로)3
+
+        // 2차원 배열의 길이를 활용
+        for (int row = 0; row < arr.length; row++)
+        {
+            for (int column = 0; column < arr[row].length; column++)
+            {
+                System.out.print(arr[row][column] + " "); // 0열 출력
+            }
+            System.out.println(); // 한 행이 끝나면 라인을 변경한다.
+        }
+    }
+}
