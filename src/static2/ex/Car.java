efa43f733f04e135842763ea8087bda55944cc40
yunjeong <yonjeong5180@nate.com>
2025-11-28 19:32:20 +0900

    25.11.28.pm

--- src/static2/ex/Car.java
+++ src/static2/ex/Car.java
@@ -2,14 +2,18 @@ package static2.ex;
 
 public class Car
 {
+    private static int totalCars;
     private String carName;
-    private static int showTotalCars;
 
 
     public Car(String carName) {
+        System.out.println("차량 구입, 이름: " + carName);
+        this.carName = carName;
+        totalCars++;
     }
 
     public static void showTotalCars()
     {
+        System.out.println("구매한 차량: " + totalCars);
     }
 }
